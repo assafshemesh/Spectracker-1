e309bed3dadcf0d9b805d77018e12c0746697e9d
Keren R <intheory@walla.com>
2020-05-07 16:23:12 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import TherapistScreen from './screens/TherapistScreen';
+import TherapistGoalsScreen from './screens/TherapistGoalsScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Therapist Home" component={TherapistScreen} />
+        <Stack.Screen name="Therapist Goals" component={TherapistGoalsScreen} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+};
+
+export default App;
